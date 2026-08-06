766d4e1eb6734c57e648e4e71fa4fae5511ae896
juandaniel <juandanielnube@gmail.com>
2026-08-06 13:07:36 -0500

    Sección de cartera balance

--- controllers/carteraBalance.controller.js
+++ controllers/carteraBalance.controller.js
@@ -3,7 +3,7 @@ const carteraBalanceService = require('../services/carteraBalance.service');
 const getResumenBalance = async (req, res, next) => {
   try {
     const { usuario } = req.params;
-    const balance = await carteraBalanceService.getResumenBalance(usuario);
+    const balance = await carteraBalanceService.getResumenBalanceByUsuario(usuario);
 
     return res.status(200).json({
       status: 'success',
--- routes/carteraBalance.routes.js
+++ routes/carteraBalance.routes.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const carteraBalanceController = require('../controllers/carteraBalance.controller');
 
-router.get('/resumen/:usuario', carteraBalanceController.getResumenBalanceByUsuario);
+router.get('/resumen/:usuario', carteraBalanceController.getResumenBalance);
 
 module.exports = router;
