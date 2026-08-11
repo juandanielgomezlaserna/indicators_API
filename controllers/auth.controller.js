8c46ef6f1063ce1ed5824852be11694b32f0273b
juandaniel <juandanielnube@gmail.com>
2026-08-10 21:02:01 -0500

    Sección de usuarios

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,9 +1,5 @@
 const authService = require('../services/auth.service');
 
-/**
- * Controller: Iniciar Sesión
- * @route POST /api/v1/auth/login
- */
 const login = async (req, res, next) => {
   try {
     const result = await authService.login(req.body);
